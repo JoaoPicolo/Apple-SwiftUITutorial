e7e9d99d4e58db67016af0c0ea9c36303f53e7ad
João Pedro Picolo <picolojoaop@gmail.com>
2021-07-05 10:22:12 -0300

    Adds list of landmarks and images

---
+++ Landmark/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmark
+//
+//  Created by João Pedro Picolo on 05/07/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
