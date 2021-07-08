9826f13f6553c62c9d1b1dc9dfc138cc6f23ac12
João Pedro Picolo <picolojoaop@gmail.com>
2021-07-08 11:14:50 -0300

    Adds MacOS files

---
+++ MacLandmark/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmark
+//
+//  Created by João Pedro Picolo on 07/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
